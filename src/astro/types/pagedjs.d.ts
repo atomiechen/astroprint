19a33cfca8333384d90ab94ca5de8f606420c9e9
Atomie CHEN <atomic_cwh@163.com>
2026-05-17 00:51:28 +0800

    refactor: move pagedjs ts type into local definition inside PrintPreview

--- src/astro/types/pagedjs.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "pagedjs" {
-  export class Previewer {
-    preview(
-      content?: Node,
-      stylesheets?: Array<string | Record<string, string>>,
-      renderTo?: Element,
-    ): Promise<{ pages: unknown[] }>;
-  }
-}
